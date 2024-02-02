4b43d6366c59df7e8ab042676f7f6e5032ab3e39
Florian Berding <florian.berding@uni-hamburg.de>
2024-02-02 22:35:36 +0100

    Fix in gpu support for embed with pytorch

--- R/text_embedding_model.R
+++ R/text_embedding_model.R
@@ -1142,9 +1142,9 @@ TextEmbeddingModel<-R6::R6Class(
             tokens$encodings$set_format(type="tensorflow")
 
             tensor_embeddings<-private$transformer_components$model(
-              input_ids=tokens$encodings["input_ids"]$to(pytorch_device),
-              attention_mask=tokens$encodings["attention_mask"]$to(pytorch_device),
-              token_type_ids=tokens$encodings["token_type_ids"]$to(pytorch_device),
+              input_ids=tokens$encodings["input_ids"],
+              attention_mask=tokens$encodings["attention_mask"],
+              token_type_ids=tokens$encodings["token_type_ids"],
               output_hidden_states=TRUE)$hidden_states
           } else {
             #Clear memory
@@ -1157,9 +1157,9 @@ TextEmbeddingModel<-R6::R6Class(
 
             with(data=torch$no_grad(),
             tensor_embeddings<-private$transformer_components$model(
-              input_ids=tokens$encodings["input_ids"],
-              attention_mask=tokens$encodings["attention_mask"],
-              token_type_ids=tokens$encodings["token_type_ids"],
+              input_ids=tokens$encodings["input_ids"]$to(pytorch_device),
+              attention_mask=tokens$encodings["attention_mask"]$to(pytorch_device),
+              token_type_ids=tokens$encodings["token_type_ids"]$to(pytorch_device),
               output_hidden_states=TRUE)$hidden_states
             )
           }
