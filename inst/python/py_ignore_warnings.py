3c260342c4288769ea293d1dcb9ce2e750eac218
yuliiatykhonova <yuliia.tykhonova@uni-hamburg.de>
2025-05-23 14:16:11 +0200

    refactor: Change ignore_data_collator_warnings function

--- inst/python/py_ignore_warnings.py
+++ inst/python/py_ignore_warnings.py
@@ -1,4 +1,8 @@
 import warnings
 
 def ignore_data_collator_warnings():
-  warnings.filterwarnings("ignore", message="DataCollatorForWholeWordMask is only suitable for BertTokenizer-like tokenizers")
+  warnings.filterwarnings(
+    "ignore",
+    message=r"^DataCollatorForWholeWordMask is only suitable for BertTokenizer-like tokenizers\.",
+    category=UserWarning,
+    module="transformers.data.data_collator")
