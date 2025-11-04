437b73491db647053c345bab197dd9b59b1bd4ad
Florian Berding <florian.berding@uni-hamburg.de>
2025-11-04 13:39:12 +0100

    Fix error in BaseModelCore

--- R/obj_BaseModelCore.R
+++ R/obj_BaseModelCore.R
@@ -1031,7 +1031,7 @@ BaseModelCore <- R6::R6Class(
       results[1L, "n_epochs"] <- n_epoch
 
       results[1L, "package"] <- "calflops"
-      if(is_venv){
+      if(is_venv()){
         results[1L, "version"] <- get_py_package_version("calflops")
       } else {
         results[1L, "version"] <- NA
