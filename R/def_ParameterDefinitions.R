4607a10a53f4736f7650741bb0aa5bba3bde2e73
Florian Berding <florian.berding@uni-hamburg.de>
2025-12-21 08:33:10 +0100

    Test run Norms

--- R/def_ParameterDefinitions.R
+++ R/def_ParameterDefinitions.R
@@ -1420,7 +1420,7 @@ get_param_dict <- function() {
     min = NULL,
     max = NULL,
     allow_null = FALSE,
-    allowed_values = c("LayerNorm", "BatchNorm","RMSNorm","PowerNorm","None"),
+    allowed_values = c("LayerNorm", "RMSNorm","None"),
     values_desc = list(
       LayerNorm = "Applies normalization as described by [Ba, Kiros, and Hinton (2016)](https://doi.org/10.48550/arXiv.1607.06450). Implementation supports masking of sequences.",
       BatchNorm = "Applies normalization as described by [Loffe and Szegedy](https://doi.org/10.48550/arXiv.1502.03167). Implementation supports masking of sequences.",
