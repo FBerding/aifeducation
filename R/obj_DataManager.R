687db75b17850af93b58431b50fdb67fe0ecb525
Florian Berding <florian.berding@uni-hamburg.de>
2026-05-07 10:12:25 +0200

    Imoprove check for calculating the number of folds

--- R/obj_DataManager.R
+++ R/obj_DataManager.R
@@ -577,17 +577,24 @@ DataManagerClassifier <- R6::R6Class(
       freq_cat <- table(sample_target)
       min_freq <- min(freq_cat)
       if (min_freq < 6L) {
-        stop("Frequency of the smallest category respective class is", min_freq, ". At least
-                   6 cases are necessary. Consider to remove this category/class.")
+        stop(
+          "Frequency of the smallest category respective class is", min_freq,". ",
+        "At least 6 cases are necessary. Consider to remove this category/class."
+        )
       } else {
         if (min_freq / folds < 4L) {
           fin_k_folds <- floor(min_freq / 4)
           if (fin_k_folds == 0L) {
-            stop("Frequency of the smallest category/label is to low. Please check your data.
-           Consider to remove all categories/labels with a very low absolute frequency.")
+            stop(
+              "Frequency of the smallest category/label is to low. Please check your data. ",
+              "Consider to remove all categories/labels with a very low absolute frequency."
+              )
           } else {
-            message("Frequency of the smallest category/label is not sufficent to ensure
-                  at least 4 cases per fold. Adjusting number of folds from ", folds, " to ", fin_k_folds, ".")
+            message(
+            "Frequency of the smallest category/label is not sufficent to ensure ",
+            "at least 4 cases per fold. Adjusting number of folds from ",
+            folds, " to ", fin_k_folds, "."
+            )
           }
         } else {
           fin_k_folds <- folds
