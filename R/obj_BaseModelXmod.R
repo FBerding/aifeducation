aa1b7f6d2bf639a1ac6cac5bc609124661fe1926
Florian Berding <florian.berding@uni-hamburg.de>
2026-05-11 20:25:52 +0200

    Add BaseModelXMod

---
+++ R/obj_BaseModelXmod.R
@@ -0,0 +1,127 @@
+# This file is part of the R package "aifeducation".
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 3 as published by
+# the Free Software Foundation.
+#
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>
+
+#' @title Xmod-Transformer
+#' @description Represents models based on Xmod
+#' @references Devlin, J., Chang, M.‑W., Lee, K., & Toutanova, K. (2019). BERT: Pre-training of Deep Bidirectional
+#'   Transformers for Language Understanding. In J. Burstein, C. Doran, & T. Solorio (Eds.), Proceedings of the 2019
+#'   Conference of the North (pp. 4171--4186). Association for Computational Linguistics. \doi{10.18653/v1/N19-1423}
+#' @return `r get_description("return_object")`
+#' @family Base Model
+#' @export
+BaseModelXmod <- R6::R6Class(
+  classname = "BaseModelXmod",
+  inherit = BaseModelCore,
+  private = list(
+    model_type = "xmod",
+    adjust_max_sequence_length = 4L,
+    create_model = function(args) {
+      configuration <- transformers$XmodConfig(
+        vocab_size = as.integer(length(args$tokenizer$get_tokenizer()$get_vocab())),
+        hidden_size = as.integer(args$hidden_size),
+        num_hidden_layers = as.integer(args$num_hidden_layers),
+        num_attention_heads = as.integer(args$num_attention_heads),
+        intermediate_size = as.integer(args$intermediate_size),
+        hidden_act = tolower(args$hidden_act),
+        hidden_dropout_prob = args$hidden_dropout_prob,
+        attention_probs_dropout_prob = args$attention_probs_dropout_prob,
+        max_position_embeddings = as.integer(args$max_position_embeddings),
+        type_vocab_size = 2L,
+        initializer_range = 0.02,
+        layer_norm_eps = 1e-12,
+        pad_token_id = as.integer(args$tokenizer$get_tokenizer()["pad_token_id"]),
+        bos_token_id =as.integer(args$tokenizer$get_tokenizer()["bos_token_id"]),
+        eos_token_id =as.integer(args$tokenizer$get_tokenizer()["eos_token_id"]),
+        use_cache = TRUE,
+        classifier_dropout = 0.1,
+        pre_norm = FALSE,
+        adapter_reduction_factor = 2L,
+        adapter_layer_norm =FALSE,
+        adapter_reuse_layer_norm=TRUE,
+        ln_before_adapter =TRUE,
+        languages = reticulate::tuple(args$languages,convert = FALSE),
+        default_language = if(is.null(args$default_language)){args$languages[1L]} else{args$default_language},
+        is_decoder=FALSE,
+        add_cross_attention =FALSE,
+        tie_word_embeddings = TRUE
+      )
+      private$model <- transformers$XmodForMaskedLM(configuration)
+    },
+    load_BaseModel = function(dir_path) {
+      tmp_model <- transformers$XmodForMaskedLM$from_pretrained(dir_path)
+      private$model=tmp_model
+      supported_languages=tmp_model$config$languages
+      tmp_model$set_default_language(supported_languages[1L])
+      message("Load model with ",supported_languages[1L]," as default language.")
+      return(private$model)
+    },
+    check_arg_combinations = function(args) {
+      if (args$hidden_size %% args$num_attention_heads != 0L) {
+        stop("hidden_size must be a multiple auf num_attention_heads.")
+      }
+    }
+  ),
+  public = list(
+    #---------------------------------------------------------------------------
+    #' @description Configures a new object of this class.
+    #' Please ensure that your chosen configuration comply with the following
+    #' guidelines:
+    #' * hidden_size is a multiple of num_attention_heads.
+    #'
+    #' @param tokenizer `r get_param_doc_desc("tokenizer")`
+    #' @param max_position_embeddings `r get_param_doc_desc("max_position_embeddings")`
+    #' @param languages `r get_param_doc_desc("languages")`
+    #' @param hidden_size `r get_param_doc_desc("hidden_size")`
+    #' @param num_hidden_layers `r get_param_doc_desc("num_hidden_layers")`
+    #' @param num_attention_heads `r get_param_doc_desc("num_attention_heads")`
+    #' @param intermediate_size `r get_param_doc_desc("intermediate_size")`
+    #' @param hidden_act `r get_param_doc_desc("hidden_act")`
+    #' @param hidden_dropout_prob `r get_param_doc_desc("hidden_dropout_prob")`
+    #' @param attention_probs_dropout_prob `r get_param_doc_desc("attention_probs_dropout_prob")`
+    #' @return `r get_description("return_nothing")`
+    configure = function(tokenizer,
+                         languages = c("eng","deu"),
+                         default_language = "deu",
+                         max_position_embeddings = 512L,
+                         hidden_size = 768L,
+                         num_hidden_layers = 12L,
+                         num_attention_heads = 12L,
+                         intermediate_size = 3072L,
+                         hidden_act = "GELU",
+                         hidden_dropout_prob = 0.1,
+                         attention_probs_dropout_prob = 0.1) {
+      arguments <- get_called_args(n = 1L)
+      private$do_configuration(args = arguments)
+    },
+    #---------------------------------------------------------------------------
+    #' @description Get the supported languages of the model.
+    #' @param language_code `string` Language code to use as default.
+    #' @return Returns a `vector` of `string`s that represent the supported
+    #' language codes.
+    get_supported_languages=function(){
+      languages=reticulate::py_to_r(private$model$config$languages)
+      return(languages)
+    },
+    #---------------------------------------------------------------------------
+    #' @description Set the default language of the model.
+    #' @param language_code `string` Language code to use as default.
+    #' @return `r get_description("return_nothing")`
+    set_default_language=function(language_code){
+      private$model$set_default_language(language_code)
+    }
+  )
+)
+
+# Add the model to the user list
+BaseModelsIndex$Xmod <- ("BaseModelXmod")
