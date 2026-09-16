62d6552004e226431475a63ed99c7a5258f2aa18
Florian Berding <florian.berding@uni-hamburg.de>
2026-09-16 10:13:39 +0200

    Update speed of dataloaders

--- inst/python/pytorch_cls_training_loops.py
+++ inst/python/pytorch_cls_training_loops.py
@@ -298,6 +298,7 @@ class ModelTrainer():
       pin_memory=pin_memory,
       drop_last =True,
       num_workers=0,
+      persistent_workers=True,
       sampler=get_sampler(train_data,ddp_use=ddp_use,rank=self.device,world_size=self.world_size),
       shuffle=not ddp_use)
     if not (val_data is None):
@@ -305,6 +306,7 @@ class ModelTrainer():
         val_data,
         batch_size=batch_size,
         pin_memory=pin_memory,
+        persistent_workers=True,
         drop_last =True,
         num_workers=0,
         sampler=get_sampler(val_data,ddp_use=ddp_use,rank=self.device,world_size=self.world_size),
@@ -316,6 +318,7 @@ class ModelTrainer():
         test_data,
         batch_size=batch_size,
         pin_memory=pin_memory,
+        persistent_workers=True,
         drop_last =True,
         num_workers=0,
         sampler=get_sampler(test_data,ddp_use=ddp_use,rank=self.device,world_size=self.world_size),
@@ -335,11 +338,13 @@ class ModelTrainer():
     trainloader=torch.utils.data.DataLoader(
       train_data,
       pin_memory = pin_memory,
+      persistent_workers=True,
       batch_sampler=ProtoNetSampler_Train)
     if not (val_data is None):
       valloader=torch.utils.data.DataLoader(
         val_data,
         pin_memory = pin_memory,
+        persistent_workers=True,
         batch_size=Ns+Nq,
         drop_last=True,
         shuffle=False)
@@ -349,6 +354,7 @@ class ModelTrainer():
       testloader=torch.utils.data.DataLoader(
         test_data,
         pin_memory =pin_memory,
+        persistent_workers=True,
         batch_size=Ns+Nq,
         drop_last=True,
         shuffle=False)
