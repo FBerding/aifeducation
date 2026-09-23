77928fabb69adf49a27f5a607971ddac8a9315eb
Florian Berding <florian.berding@uni-hamburg.de>
2026-09-23 14:04:08 +0200

    Update test for feature extractor

--- tests/testthat/test-03_05_FeatureExtractor.R
+++ tests/testthat/test-03_05_FeatureExtractor.R
@@ -34,7 +34,7 @@ dataset_list <- list(
 ml_frameworks <- c("pytorch")
 
 method_list <- list(
-  "pytorch" = c("Conv")
+  "pytorch" = c("Dense","DenseTimes")
 )
 
 # Start tests--------------------------------------------------------------------
